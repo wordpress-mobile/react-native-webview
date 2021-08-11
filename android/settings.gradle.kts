6f802888c408204bab11847396cf8e4414111c74
Oguz Kocer <oguz.kocer@automattic.com>
2021-08-11 11:36:10 -0400

    Simplify android extension

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,7 +1,9 @@
 pluginManagement {
+    gradle.ext.kotlinVersion = "1.5.20"
+
     plugins {
         id("com.android.library") version "4.2.2"
-        id "org.jetbrains.kotlin.android" version "1.5.20"
+        id "org.jetbrains.kotlin.android" version gradle.ext.kotlinVersion
     }
     repositories {
         gradlePluginPortal()

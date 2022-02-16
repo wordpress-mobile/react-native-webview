1f8a670c7dce595b1e3973e2810602a76ec51bc4
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-02-16 13:16:22 +0200

    Build: Update agp to 7.0.4

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("com.android.library") version "4.2.2"
+        id("com.android.library") version "7.0.4"
         id("org.jetbrains.kotlin.android") version "1.5.20"
     }
     repositories {
